8f58a9e5533188e275816658143c766b4e9113d9
Bernadett Ignácz <60573071+dignacz@users.noreply.github.com>
2022-04-29 22:25:14 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,108 @@
+<div class="container mt-5">
+<?php
+include('server.php');
+if(!empty($_POST["send"])) {
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+$sql = "INSERT INTO contacts_list (name, email, phone, subject, message, sent_date)
+VALUES ('{$name}', '{$email}', '{$phone}', '{$subject}', '{$message}', now())";
+mysqli_query($db, $sql);
+if(!$sql) {
+die("MySQL query failed.");
+} else {
+header('location: index.php?page=contactshow');  
+}
+}  
+?>
+<!-- Messge -->
+<?php if(!empty($response)) {?>
+<div class="alert text-center <?php echo $response['status']; ?>" role="alert">
+<?php echo $response['message']; ?>
+</div>
+<?php }?>
+<!-- Contact form -->
+<div class="row">
+<h3>Kapcsolat</h3>
+<form action="" name="contactForm" method="post" enctype="multipart/form-data">
+<div class="form-group">
+<label>Név</label>
+<input type="text" class="form-control" name="name" id="name">
+</div>
+<div class="form-group">
+<label>Email</label>
+<input type="email" class="form-control" name="email" id="email">
+</div>
+<div class="form-group">
+<label>Telefon (06 formátumban)</label>
+<input type="text" class="form-control" name="phone" id="phone">
+</div>
+<div class="form-group">
+<label>Tárgy</label>
+<input type="text" class="form-control" name="subject" id="subject">
+</div>
+<div class="form-group">
+<label>Üzenet</label>
+<textarea class="form-control" name="message" id="message" rows="4"></textarea>
+</div>
+<input type="submit" name="send" value="Elküld" class="btn btn-dark btn-block">
+</form>
+</div>
+</div>
+<!-- JavaScript -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.2/dist/jquery.validate.min.js"></script>
+<script>
+$(function() {
+$("form[name='contactForm']").validate({
+// Define validation rules
+rules: {
+name: "required",
+email: "required",
+phone: "required",
+subject: "required",
+message: "required",
+name: {
+required: true
+},
+email: {
+required: true,
+email: true
+},          
+phone: {
+required: true,
+minlength: 10,
+maxlength: 11,
+number: true
+},          
+subject: {
+required: true
+},          
+message: {
+required: true
+}
+},
+// Specify validation error messages
+messages: {
+name: "Kérlek valós nevet adj meg.",
+email: {
+required: "Kérlek írd be az email címed.",
+minlength: "Kérlek valódi email címet adj meg."
+},
+phone: {
+required: "Kérlek add meg a telefonszámodat.",
+minlength: "A telefonszám minimum 10 karakter lehet.",
+maxlength: "A telefonszám nem haladhatja meg a 11 karaktert."
+},
+subject: "Kérlek add meg a levél tárgyát!",
+message: "Kérlek írd be az üzenetet!"
+},
+submitHandler: function(form) {
+form.submit();
+}
+});
+});    
+</script>
---
+++ home.php
@@ -0,0 +1,20 @@
+<div class ="donate">
+<img src="images/mopsz-1-szazalek-tamogatas-banner.jpg" style="width:100%">
+</div>
+
+<!-- Embed video -->
+<div class="container">
+  <div class="row">
+  <div class="col-sm">
+<iframe width="320" height="180" src="video/dubstepbird.mp4" frameborder="0" allowfullscreen sandbox></iframe>
+      </div>
+      <div class="col-sm">
+<iframe width="320" height="180" src="https://www.youtube.com/embed/1Hxq0IQY9lA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+ 
+
+<!--Embed Google maps-->
+<iframe width="80%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2696.8047885350734!2d19.035876715253586!3d47.474233405106304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741ddb6f651012d%3A0x174eb65d148247b9!2zQnVkYXBlc3QsIEJhcnTDs2sgQsOpbGEgw7p0IDkyLTk0YSwgMTExNQ!5e0!3m2!1sen!2shu!4v1651230446442!5m2!1sen!2shu" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+</div>
+</div>
